c27709bb9e682903a1b7f28e79f05e69f5612f5b
Polina Stepanova <stepa.polina24@gmail.com>
2023-05-09 15:29:25 +0200

    updated data cleaning code
    Updated code, which replaces NULL value with N/A and deletes text duplicates

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,21 +7,20 @@ cnx = sqlite3.connect(path_db)
 # replace null values in multiple columns with "N/A"
 query_update = '''
     UPDATE twitter_data 
-    SET created_at = COALESCE(created_at, 'N/A'),
-        id = COALESCE(id, 'N/A'),
-        id_str = COALESCE(id_str, 'N/A'),
+    SET 
+        id_str = COALESCE(id_str, 'N/A') ,
         text = COALESCE(text, 'N/A'),
-        source = COALESCE(source, 'N/A'),
-        truncated = COALESCE(source, 'N/A'),
+        source  = COALESCE(source, 'N/A'),
+        truncated = COALESCE(truncated, 'N/A'),
+        in_reply_to_status_id = COALESCE(in_reply_to_status_id, 'N/A'),
+        in_reply_to_status_id_str = COALESCE(in_reply_to_status_id_str, 'N/A'),
+        in_reply_to_user_id = COALESCE(in_reply_to_user_id, 'N/A'),
+        in_reply_to_user_id_str = COALESCE(in_reply_to_user_id_str, 'N/A'),
+        in_reply_to_screen_name = COALESCE(in_reply_to_screen_name, 'N/A'),
         user_id = COALESCE(user_id, 'N/A'),
-        user_name = COALESCE(user_name, 'N/A'),
-        user_screen_name = COALESCE(user_screen_name, 'N/A'),
-        user_location = COALESCE(truncated, 'N/A'),
-        user_description = COALESCE(user_description, 'N/A'),
-        user_verified = COALESCE(user_verified, 'N/A'),
-        user_followers_count = COALESCE(user_followers_count, 0),
-        user_friends_count = COALESCE(user_friends_count, 0),
-        user_statuses_count = COALESCE(user_statuses_count, 0)
+        extended_tweet = COALESCE(extended_tweet, 'N/A'),
+        entities = COALESCE(entities, 'N/A');
+        
 '''
 
 # remove duplicates from the 'text' column
